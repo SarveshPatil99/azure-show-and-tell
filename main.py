0b3e8e8d0c03eff2b33b0eb3ee81d65ac6e7ffc9
Sarvesh <steadycraft@users.noreply.github.com>
2018-09-15 18:53:53 +0530

    added some comments

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
+# Flask imports
 import os
 from flask import Flask, render_template, send_from_directory, send_file, request, url_for, jsonify, redirect, Request, g
 from io import StringIO
 from werkzeug import secure_filename
-from tts import sapi
 
+# TTS imports
+from tts import sapi
+import pythoncom
+
+# Show-and-Tell imports
 import math
 import heapq
 import numpy as np
-import os
 import tensorflow as tf
 from tensorflow.contrib.slim.python.slim.nets.inception_v3 import inception_v3_base
-
 from configuration import ModelConfig
 from inference_wrapper import InferenceWrapper
 from inference_utils.caption_generator import CaptionGenerator
 from inference_utils.vocabulary import Vocabulary
 
+
 FLAGS = tf.flags.FLAGS
 slim = tf.contrib.slim
 
+# Setting up the voice for TTS
 voice = sapi.Sapi()
 
 
 def del_all_flags(FLAGS):
+    """
+    Deletes all the flags to avoid the DuplicateFlagError
+    """
     flags_dict = FLAGS._flags()
     keys_list = [keys for keys in flags_dict]
     for keys in keys_list:
@@ -31,17 +39,15 @@ def del_all_flags(FLAGS):
     
 
 del_all_flags(FLAGS)
-tf.flags.DEFINE_string("checkpoint_path", "./model.ckpt-2000000",
-                   "Model checkpoint file or directory containing a "
-                   "model checkpoint file.")
+# Path to 
+tf.flags.DEFINE_string("checkpoint_path", "./model.ckpt-2000000", "Model checkpoint file.")
 tf.flags.DEFINE_string("vocab_file", "./word_counts.txt", "Text file containing the vocabulary.")
 tf.logging.set_verbosity(tf.logging.INFO)
 # Build the inference graph.
 g = tf.Graph()
 with g.as_default():
     model = InferenceWrapper()
-    restore_fn = model.build_graph_from_config(ModelConfig(),
-                                               FLAGS.checkpoint_path)
+    restore_fn = model.build_graph_from_config(ModelConfig(), FLAGS.checkpoint_path)
 g.finalize()
 vocab = Vocabulary(FLAGS.vocab_file)
 sess = tf.InteractiveSession(graph=g)
@@ -53,6 +59,7 @@ os.environ[ 'MPLCONFIGDIR' ] = '/tmp/'
 
 UPLOAD_FOLDER = os.path.join(app.root_path, 'uploads')
 STATIC_FOLDER = os.path.join(app.root_path, 'static')
+# Read count.txt and initialize count
 with open('count.txt') as f:
     count  = int(next(f))
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif','PNG','JPEG','JPG'])
@@ -75,12 +82,10 @@ def upload_file():
             filename=secure_filename(file.filename)
             uploadFilePath=os.path.join(app.config['UPLOAD_FOLDER'],filename)
             file.save(uploadFilePath)
-            print(filename)
             tf.flags.DEFINE_string("input_files", "./uploads/" + filename,"Image path.")
             with tf.gfile.GFile(FLAGS.input_files, "rb") as f:
                 image = f.read()
                 captions = generator.beam_search(sess, image)
-                img_caption = "Caption for image %s:" % os.path.basename(FLAGS.input_files)
                 caps = []
                 for i, caption in enumerate(captions):
                     # Ignore begin and end words.
@@ -88,17 +93,13 @@ def upload_file():
                     sentence = " ".join(sentence)
                     caps.append(sentence)
                 FLAGS.__delattr__('input_files')
-            import pythoncom
             pythoncom.CoInitialize()
-            if os.path.exists("output.wav"):
-                os.remove("output.wav")
-            print(app.root_path)
+            # Creates .wav file saying the caption
             voice.create_recording('output' + str(count) + '.wav', caps[0])
             count += 1
+            # Update count.txt
             with open('count.txt', 'w') as f:
                 f.write(str(count))
-            while(not os.path.exists('output' + str(count-1) + '.wav')):
-                pass
             return render_template('render.html', filename = 'uploads/' + filename, cap1 = caps[0], cap2 = caps[1], cap3 = caps[2])
     return render_template('index.html')
 
